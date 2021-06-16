4338f26080da8900d8d5f4011c4fded8768c40b9
akashashok56 <akashashok56@gmail.com>
2021-06-16 09:15:31 +0530

    commited

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+//adding styles
+
+
+//adding component
+
+import Player from './components/Player';
+import Song from './components/Song';
+import "./styles/app.scss";
+function App() {
+  return (
+    <div className="App">
+
+
+      <Song />
+      <Player />
+
+
+    </div>
+
+
+  );
+}
+
+export default App;
---
+++ src/components/Song.js
@@ -0,0 +1,25 @@
+
+import React from "react";
+
+
+const song = () => {
+
+    return (
+        <div className="song-container" >
+            <h1>picture</h1>
+            <h1>song name</h1>
+            <h1>Artist</h1>
+
+
+
+        </div>
+
+
+    );
+};
+export default song;
+
+
+
+
+
